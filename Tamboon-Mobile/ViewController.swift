21c7b727747835e1ab06ddc88ce4cdbec3b308dd
Milion <milionsun@hotmail.com>
2017-12-14 15:58:56 +0700

    Initial Commit

---
+++ Tamboon-Mobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tamboon-Mobile
+//
+//  Created by Milion Sun on 12/14/2560 BE.
+//  Copyright © 2560 Chayanon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
